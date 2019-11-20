480e6ae801ab405f0d610d648a4cff88d82c2e6a
Sivabudh Umpudh <sivabudh.u@codium.co>
2019-11-20 21:35:50 +0700

    Finished pubsub

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from pubsub import PubSub
 
-app = Celery('periodic')
+app = Celery()
 pubsub_app = PubSub('amqp://guest@localhost', 'sample.topic')
 
 
@@ -11,14 +11,11 @@ def setup_periodic_tasks(sender, **kwargs):
 
 
 @pubsub_app.subscribe('booking.*')
-def process_booking_event(_, message):
-    print("Subscriber!")
-    print(message)
+def process_booking_event(body, _):
+    print(body['object'])
 
 
 @app.task
 def pubsub_listen():
-    print("Go go")
     pubsub_app.drain()
-    print("Drained")
     return "Done!"
